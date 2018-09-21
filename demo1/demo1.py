fc5f13587204d37bc870fcfeb0a2192b78c34505
mingya.zhang <mingya.zhang@hobot.cc>
2018-09-21 10:05:53 +0800

    新建deMo

---
+++ demo1/demo1.py
@@ -0,0 +1,21 @@
+from flask import  Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    url_str = 'www.baidu.com'
+
+    my_list = [1,3,4,7]
+
+    my_dict = {
+        "name":"simzhng",
+        "url":"www.baidu.com"
+    }
+
+    my_int = 33
+    # 通常 模板中使用的变量名和要传递的数据名 保持一致
+    return render_template('index.html', url_str=url_str, my_list=my_list, my_dict = my_dict, my_int = my_int)
+
+if __name__ == '__main__':
+    app.run(debug=True)
